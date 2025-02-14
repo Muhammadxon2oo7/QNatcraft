5d3173dd2166d5c48c8f480f68eaf7ca69a23ef3
Muhammadxon2oo7 <muhammadxon0709@gmail.com>
2025-02-14 14:48:25 +0500

    360

---
+++ src/app/korgazmalar/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Korgazmalar = () => {
+  return (
+    <div className=' mt-[200px] flex flex-wrap gap-[15px]'>
+       <iframe
+        width="90%"
+        height="500"
+        allowFullScreen
+        allow="accelerometer; magnetometer; gyroscope"
+        className="block mx-auto border-none max-w-[880px] rounded-lg shadow-lg"
+        src="https://panoraven.com/en/embed/PIw3vFvzJ0"
+      ></iframe>
+    </div>
+  )
+}
+
+export default Korgazmalar
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,54 @@
-"use client"
+// "use client"
+// import Link from "next/link";
+// import { ShoppingBag } from "lucide-react"; // Shadcn ikonasi
+// import { useTranslations } from "next-intl";
+
+// export default function Navbar() {
+//   const t=useTranslations("header")
+//   return (
+//     <nav className="primary-bg w-full flex h-[56px] items-center media">
+//      <div className="container  md:justify-start text-white font-medium gap-4 md:gap-6 lg:gap-[47px] h-full w-full flex  items-center justify-center">
+//   <Link href="#aboutus">{t("first")}</Link>
+//   <span className="hidden md:inline text-white text-opacity-20">|</span>
+//   <Link href="#madaniymeros">{t("second")}</Link>
+//   <span className="hidden md:inline text-white text-opacity-20">|</span>
+//   <Link href="#Hunarmandchilikturlari">{t("third")}</Link>
+//   <span className="hidden md:inline text-white text-opacity-20">|</span>
+//   <Link href="#Hunarmandlar">{t("fourth")}</Link>
+//   <span className="hidden md:inline text-white text-opacity-20">|</span>
+//   <Link href="/">{t("fifth")}</Link>
+//   <span className="hidden md:inline text-white text-opacity-20">|</span>
+//   <Link href="/" className="flex items-center space-x-1">
+//     <ShoppingBag size={18} /> <span>{t("sixth")}</span>
+//   </Link>
+// </div>
+
+//     </nav>
+//   );
+// }
+
+"use client";
 import Link from "next/link";
 import { ShoppingBag } from "lucide-react"; // Shadcn ikonasi
 import { useTranslations } from "next-intl";
 
 export default function Navbar() {
-  const t=useTranslations("header")
+  const t = useTranslations("header");
+
   return (
-    <nav className="primary-bg flex h-[56px] items-center ">
-      <div className="container h-[100%] flex items-center   text-white font-medium">
-        <Link href="#aboutus" className="mr-[47px]" >{t("first")}</Link>
-        <span className="text-white text-opacity-20">|</span>
-        <Link href="#madaniymeros" className="mx-[47px]">{t("second")}</Link>
-        <span className="text-white text-opacity-20">|</span>
-        <Link href="#Hunarmandchilikturlari" className="mx-[47px]">{t("third")}</Link>
-        <span className="text-white text-opacity-20">|</span>
-        <Link href="#Hunarmandlar" className="mx-[47px]">{t("fourth")}</Link>
-        <span className="text-white text-opacity-20">|</span>
-        <Link href="/" className="mx-[47px]">{t("fifth")}</Link>
-        <span className="text-white text-opacity-20">|</span>
-        <Link href="/" className="flex items-center space-x-1 ml-[47px] ">
+    <nav className="primary-bg w-full flex h-[56px] items-center">
+      <div className="container md:justify-start text-white font-medium gap-4 md:gap-6 lg:gap-[47px] h-full w-full flex items-center justify-center">
+        <Link href="#aboutus" className="text-sm md:text-base lg:text-lg">{t("first")}</Link>
+        <span className="hidden md:inline text-white text-opacity-20">|</span>
+        <Link href="#madaniymeros" className="text-sm md:text-base lg:text-lg">{t("second")}</Link>
+        <span className="hidden md:inline text-white text-opacity-20">|</span>
+        <Link href="#Hunarmandchilikturlari" className="text-sm md:text-base lg:text-lg">{t("third")}</Link>
+        <span className="hidden md:inline text-white text-opacity-20">|</span>
+        <Link href="#Hunarmandlar" className="text-sm md:text-base lg:text-lg">{t("fourth")}</Link>
+        <span className="hidden md:inline text-white text-opacity-20">|</span>
+        <Link href="/" className="text-sm md:text-base lg:text-lg">{t("fifth")}</Link>
+        <span className="hidden md:inline text-white text-opacity-20">|</span>
+        <Link href="/" className="flex items-center space-x-1 text-sm md:text-base lg:text-lg">
           <ShoppingBag size={18} /> <span>{t("sixth")}</span>
         </Link>
       </div>
