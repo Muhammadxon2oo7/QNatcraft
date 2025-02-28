de4b7b81e486d30b163786c28af6df58435c3904
Muhammadxon2oo7 <muhammadxon0709@gmail.com>
2025-02-28 16:34:34 +0500

    zoom

---
+++ public/store/pdp/Group.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export const Group = () => {
+  return (
+<svg width={18} height={19} viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M11.625 6.29163C11.625 7.74137 10.4497 8.91663 9 8.91663C7.55025 8.91663 6.375 7.74137 6.375 6.29163C6.375 4.84188 7.55025 3.66663 9 3.66663C10.4497 3.66663 11.625 4.84188 11.625 6.29163Z" fill="url(#paint0_linear_2161_9751)" />
+  <path d="M13.5 13.0416C13.5 14.4914 11.4853 15.6666 9 15.6666C6.51472 15.6666 4.5 14.4914 4.5 13.0416C4.5 11.5919 6.51472 10.4166 9 10.4166C11.4853 10.4166 13.5 11.5919 13.5 13.0416Z" fill="url(#paint1_linear_2161_9751)" />
+  <path d="M5.34154 4.41663C5.47463 4.41663 5.60457 4.42969 5.73004 4.45455C5.42437 4.99747 5.25 5.62418 5.25 6.29163C5.25 6.94282 5.41598 7.55524 5.70795 8.0889C5.58934 8.111 5.46684 8.12257 5.34154 8.12257C4.28073 8.12257 3.42077 7.29297 3.42077 6.2696C3.42077 5.24623 4.28073 4.41663 5.34154 4.41663Z" fill="url(#paint2_linear_2161_9751)" />
+  <path d="M4.0855 14.9061C3.65957 14.3969 3.375 13.7721 3.375 13.0416C3.375 12.3334 3.64243 11.7246 4.04684 11.2242C2.61832 11.335 1.5 12.1163 1.5 13.0637C1.5 14.0199 2.63798 14.807 4.0855 14.9061Z" fill="url(#paint3_linear_2161_9751)" />
+  <path d="M12.7496 6.29163C12.7496 6.94282 12.5836 7.55524 12.2916 8.0889C12.4102 8.111 12.5327 8.12257 12.658 8.12257C13.7188 8.12257 14.5788 7.29297 14.5788 6.2696C14.5788 5.24623 13.7188 4.41663 12.658 4.41663C12.5249 4.41663 12.395 4.42969 12.2695 4.45455C12.5752 4.99747 12.7496 5.62418 12.7496 6.29163Z" fill="url(#paint4_linear_2161_9751)" />
+  <path d="M13.9141 14.9061C15.3616 14.807 16.4996 14.0199 16.4996 13.0637C16.4996 12.1163 15.3812 11.335 13.9527 11.2242C14.3571 11.7246 14.6246 12.3334 14.6246 13.0416C14.6246 13.7721 14.34 14.3969 13.9141 14.9061Z" fill="url(#paint5_linear_2161_9751)" />
+  <defs>
+    <linearGradient id="paint0_linear_2161_9751" x1="11.625" y1="3.66663" x2="5.32692" y2="5.75335" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#9E1114" />
+      <stop offset={1} stopColor="#530607" />
+    </linearGradient>
+    <linearGradient id="paint1_linear_2161_9751" x1="13.5" y1="10.4166" x2="4.44069" y2="15.5622" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#9E1114" />
+      <stop offset={1} stopColor="#530607" />
+    </linearGradient>
+    <linearGradient id="paint2_linear_2161_9751" x1="5.73004" y1="4.41663" x2="0.197248" y2="5.15588" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#9E1114" />
+      <stop offset={1} stopColor="#530607" />
+    </linearGradient>
+    <linearGradient id="paint3_linear_2161_9751" x1="5.73004" y1="4.41663" x2="0.197248" y2="5.15588" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#9E1114" />
+      <stop offset={1} stopColor="#530607" />
+    </linearGradient>
+    <linearGradient id="paint4_linear_2161_9751" x1="16.4996" y1="4.41663" x2="10.9668" y2="5.15588" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#9E1114" />
+      <stop offset={1} stopColor="#530607" />
+    </linearGradient>
+    <linearGradient id="paint5_linear_2161_9751" x1="16.4996" y1="4.41663" x2="10.9668" y2="5.15588" gradientUnits="userSpaceOnUse">
+      <stop stopColor="#9E1114" />
+      <stop offset={1} stopColor="#530607" />
+    </linearGradient>
+  </defs>
+</svg>
+
+  )
+}
